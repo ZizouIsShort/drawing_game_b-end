1c020250cedb9e7d903f6b2d7e434c9005c76f95
Ziyan Solkar <ziyansolkar@gmail.com>
2026-03-09 23:57:13 +0530

    server receives the message and broadcasts it

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello, Node.js project!");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+
+ //pls work
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+io.on("connection", (Socket) => {
+  console.log("a user is connected with id : ", Socket.id);
+  Socket.on("chat message", (msg) => {
+    console.log(msg, Socket.id);
+    io.emit("message", msg);
+  });
+});
+
+httpServer.listen(3005);
