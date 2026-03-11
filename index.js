38c16c66d932f30175dad47c1c82d3b7376f10dd
Ziyan Solkar <ziyansolkar@gmail.com>
2026-03-11 21:42:33 +0530

    room created and messages are being sent and received

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-const { createServer } = require("http");
+const express = require("express");
+const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
+const { createServer } = require("http");
+const crypto = require("crypto");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 
- //pls work 12
-const httpServer = createServer();
+const httpServer = http.createServer(app);
+
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
   },
 });
-io.on("connection", (Socket) => {
-  console.log("a user is connected with id : ", Socket.id);
-  Socket.on("chat message", (msg) => {
-    console.log(msg, Socket.id);
-    io.emit("message", msg);
+
+app.post("/create", (req, res) => {
+  const user_id = req.userId
+  console.log(user_id)
+  const room_id = crypto.randomUUID()
+  console.log(room_id)
+  res.send(room_id)
+})
+
+io.on("connection", function(socket){
+  socket.on('join_room', function (room) {
+    socket.join(room);
+    console.log(`socket ${socket.id} joined room ${room}`);
+  });
+  socket.on("chat message", function ({ room, msg }) {
+    console.log(msg, socket.id);
+    socket.to(room).emit("message", msg);
   });
 });
 
-httpServer.listen(3005);
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+httpServer.listen(3005, () => {
+  console.log("Server listening on port 3005");
+});
