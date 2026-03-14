d91b1d9f4588d4f95132d1ba8a2783574fb896a5
Ziyan Solkar <ziyansolkar@gmail.com>
2026-03-15 01:33:05 +0530

    socket listens for draw event

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
@@ -20,21 +19,25 @@ const io = new Server(httpServer, {
 });
 
 app.post("/create", (req, res) => {
-  const user_id = req.userId
-  console.log(user_id)
-  const room_id = crypto.randomUUID()
-  console.log(room_id)
-  res.send(room_id)
-})
+  const user_id = req.userId;
+  console.log(user_id);
+  const room_id = crypto.randomUUID();
+  console.log(room_id);
+  res.send(room_id);
+});
 
-io.on("connection", function(socket){
-  socket.on('join_room', function (room) {
+io.on("connection", function (socket) {
+  socket.on("join_room", function (room) {
     socket.join(room);
     console.log(`socket ${socket.id} joined room ${room}`);
   });
   socket.on("chat message", function ({ room, msg }) {
     console.log(msg, socket.id);
     socket.to(room).emit("message", msg);
+  });
+  socket.on("draw", ({ room, art }) => {
+    console.log("DRAW EVENT:", art);
+    socket.to(room).emit("draw", art);
   });
 });
 //change test
